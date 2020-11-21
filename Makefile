510a720ea30b2857f47f7f0f2394dbcbaedd957d
John Mettraux <jmettraux@gmail.com>
2020-11-21 09:44:46 +0900

    Refine `make log`

--- Makefile
+++ Makefile
@@ -43,16 +43,11 @@ b: browse
 redate:
 	bundle exec ruby -Ilib lib/redate_post.rb posts/`ls -1 -t posts/ | head -1`
 
-backup:
-	cd .. && tar cjvf ~/Dropbox/backup/blog_`date +%Y%m%d_%H%M%S`.tbz blog/
+#backup:
+#	cd .. && tar cjvf ~/Dropbox/backup/blog_`date +%Y%m%d_%H%M%S`.tbz blog/
 
 log:
-	ssh -t shooto cat /var/www/logs/access.log | grep "^weaver" > weaver.log
-	ruby lib/log.rb < weaver.log > w.log
-	rm weaver.log
-	tail -1 w.log
-	wc -l w.log
-	mv w.log tmp/weaver_`tail -1 w.log | ruby -e "print STDIN.read.match(/\[(.+)\]/)[1].gsub(/[-:]/, '').gsub(/[^0-9]/, '_')"`.log
+	ssh -t shooto cat /var/www/logs/weaver_access.log | ruby lib/log.rb
 
 
 .PHONY: backup posts publish redate serve log
--- lib/log.rb
+++ lib/log.rb
@@ -8,12 +8,25 @@ CACHE =
     h[k] = m[1] rescue '???'
   end
 
+out = []
+
 STDIN.readlines.each do |l|
   l = l.strip
   m = l.match(/^[^ ]+ ([^ ]+) /)
   a = CACHE[m[1]]
   l = l.gsub(/\[[^\]]+\]/) { |x|
     '[' + Time.parse(x[1..-2].sub(':', ' ')).localtime.to_s + ']' }
-  puts "#{a} #{l.split(/\s+/)[1..-1].join(' ')}"
+  out << "#{a} #{l.split(/\s+/)[1..-1].join(' ')}"
 end
+
+sta = out[0].match(/\[(.+)\]/)[1].gsub(/[-:]/, '').gsub(/[^0-9]/, '_')[0, 13]
+edn = out[-1].match(/\[(.+)\]/)[1].gsub(/[-:]/, '').gsub(/[^0-9]/, '_')[0, 13]
+
+fn = "tmp/weaver__#{sta}__#{edn}.log"
+
+File.open(fn, 'wb') do |f|
+  out.each { |l| f.puts(l) }
+end
+
+puts "#{File.exist?(fn) ? 'over' : ''}wrote #{fn}"
 
