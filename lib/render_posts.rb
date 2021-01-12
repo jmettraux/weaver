b07e14745959841a0d1dfb7a1f4ca244b8949926
John Mettraux <jmettraux@gmail.com>
2021-01-12 10:06:03 +0900

    Use h-entry and h-card microformats for posts

--- lib/render_posts.rb
+++ lib/render_posts.rb
@@ -4,6 +4,7 @@ require 'blog'
 
 post_layout = File.read('layouts/post.html')
 tag_partial = File.read('partials/post-tag.html')
+meta_tag_partial = File.read('partials/post-meta-tag.html')
 
 Dir['posts/*.md'].sort.each do |path|
 
@@ -20,6 +21,9 @@ Dir['posts/*.md'].sort.each do |path|
     vars['TAGS'] = vars['tags']
       .collect { |tag| tag_partial.substitute({ tag: tag }) }
       .join(' ')
+    vars['meta_tags'] = vars['tags']
+      .collect { |tag| meta_tag_partial.substitute({ tag: tag }) }
+      .join.strip
 
     ct = content
       .split("\n")
