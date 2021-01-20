f8fc9a57bdaad71eac452bf5e9f0aa7786860c62
John Mettraux <jmettraux@gmail.com>
2021-01-20 19:38:08 +0900

    Add .prev and .next

--- lib/render_posts.rb
+++ lib/render_posts.rb
@@ -6,13 +6,36 @@ post_layout = File.read('layouts/post.html')
 tag_partial = File.read('partials/post-tag.html')
 meta_tag_partial = File.read('partials/post-meta-tag.html')
 
-Dir['posts/*.md'].sort.each do |path|
+paths = Dir['posts/*.md'].sort
+
+paths.each do |path|
 
   fn = 'out/' + File.basename(path, '.md') + '.html'
+
+  i = paths.index(path)
+  prv = paths[i - 1]
+  nxt = paths[i + 1]
+  f = lambda { |kla, path|
+      #
+    d = File.basename(path.split('/').last, '.md')
+    d1 = [ d[0, 4], d[4, 2], d[6, 2] ].join('-')
+    t = File.read(path).match(/^## (.+)\n/)[1]
+    t1 = t.gsub(/[^a-zA-Z0-9]/, '_')
+    l = "#{d}.html?f=a&t=#{t1}"
+      #
+    "<div class=\"#{kla}\">" +
+    "<a href=\"#{l}\">" +
+    "#{kla == 'prev' ? '← ' : ''}#{d1} #{t}#{kla == 'next' ? ' →' : ''}</a>" +
+    "</div>" }
+  prv = f['prev', prv] if prv
+  nxt = f['next', nxt] if nxt
 
   File.open(fn, 'wb') { |f|
 
     vars, content = Blog.load_post(path)
+
+    vars['PREV'] = prv
+    vars['NEXT'] = nxt
 
     sc = content.substitute(vars)
 
