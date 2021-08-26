8d7e11a94b761faf8fa83e3d7fa5d66482e4ec89
John Mettraux <jmettraux@gmail.com>
2021-08-26 15:05:39 +0900

    Allow for non-empty figcaption in top image

--- lib/render_index.rb
+++ lib/render_index.rb
@@ -15,6 +15,7 @@ posts =
     vars, content = Blog.load_post(path)
 
     content = content
+      .gsub(/\n<figcaption>[^<]+<\/figcaption>/, '')
       .split("\n", 2).last # remove title
       .split("\n")
       .reject { |l| l.match(/^(#|<|-->)/) }
